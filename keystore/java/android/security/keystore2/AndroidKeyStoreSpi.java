2f1269570096854ce89d6121c65088d082e841f7
Shaquille Johnson <ssjohnson@google.com>
2023-08-10 16:36:53 +0100

    [Keystore] Use Strict for every call into keystore
    This has strictmode annotations for when calls are
    made into Keystore DB to make reads or writes.
    
    Test: atest CtsKeystoreTestCases
    Bug: 180135124
    Change-Id: I819e1c63875a4af16a6fbe991a9f7c9c95ea8e6a
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    Signed-off-by: Rimuru <paleloebarbershop@gmail.com>

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -21,6 +21,7 @@ import android.hardware.biometrics.BiometricManager;
 import android.hardware.security.keymint.HardwareAuthenticatorType;
 import android.hardware.security.keymint.KeyParameter;
 import android.hardware.security.keymint.SecurityLevel;
+import android.os.StrictMode;
 import android.security.GateKeeper;
 import android.security.KeyStore2;
 import android.security.KeyStoreParameter;
@@ -87,6 +88,7 @@ import javax.crypto.SecretKey;
  * This is built on top of Android's keystore daemon. The convention of alias
  * use is:
  * <p>
+/**
  * PrivateKeyEntry will have a Credentials.USER_PRIVATE_KEY as the private key,
  * Credentials.USER_CERTIFICATE as the first certificate in the chain (the one
  * that corresponds to the private key), and then a Credentials.CA_CERTIFICATE
@@ -154,6 +156,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
         KeyDescriptor descriptor = makeKeyDescriptor(alias);
 
         try {
+            StrictMode.noteDiskRead();
             return mKeyStore.getKeyEntry(descriptor);
         } catch (android.security.KeyStoreException e) {
             if (e.getErrorCode() != ResponseCode.KEY_NOT_FOUND) {
@@ -439,6 +442,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
             assertCanReplace(alias, targetDomain, mNamespace, descriptor);
 
             try {
+                StrictMode.noteDiskWrite();
                 mKeyStore.updateSubcomponents(
                         ((AndroidKeyStorePrivateKey) key).getKeyIdDescriptor(),
                         userCertBytes, chainBytes);
@@ -559,6 +563,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
                     importArgs, flags, pkcs8EncodedPrivateKeyBytes);
 
             try {
+                StrictMode.noteDiskWrite();
                 mKeyStore.updateSubcomponents(metadata.key, userCertBytes, chainBytes);
             } catch (android.security.KeyStoreException e) {
                 mKeyStore.deleteKey(metadata.key);
@@ -869,6 +874,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
         KeyEntryResponse response = null;
         try {
+            StrictMode.noteDiskRead();
             response = mKeyStore.getKeyEntry(wrappingkey);
         } catch (android.security.KeyStoreException e) {
             throw new KeyStoreException("Failed to import wrapped key. Keystore error code: "
@@ -899,6 +905,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
         }
 
         try {
+            StrictMode.noteDiskWrite();
             securityLevel.importWrappedKey(
                     wrappedKey, wrappingkey,
                     entry.getWrappedKeyBytes(),
@@ -959,6 +966,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
         }
 
         try {
+            StrictMode.noteDiskWrite();
             mKeyStore.updateSubcomponents(makeKeyDescriptor(alias),
                     null /* publicCert - unused when used as pure certificate store. */,
                     encoded);
@@ -971,6 +979,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
     public void engineDeleteEntry(String alias) throws KeyStoreException {
         KeyDescriptor descriptor = makeKeyDescriptor(alias);
         try {
+            StrictMode.noteDiskWrite();
             mKeyStore.deleteKey(descriptor);
         } catch (android.security.KeyStoreException e) {
             if (e.getErrorCode() != ResponseCode.KEY_NOT_FOUND) {
@@ -1009,6 +1018,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
         return getKeyMetadata(alias) != null;
     }
+
 
     @Override
     public int engineSize() {
@@ -1069,6 +1079,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
         KeyDescriptor[] keyDescriptors = null;
         try {
+            StrictMode.noteDiskRead();
             keyDescriptors = mKeyStore.list(
                     getTargetDomain(),
                     mNamespace
