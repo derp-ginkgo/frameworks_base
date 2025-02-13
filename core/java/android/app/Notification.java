6df252e949d522f6bf66af47ef4e12d24e2915c6
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-13 13:34:27 +0800

    Reduce notification memory usage
    Change-Id: I75a99e79f357874cf3e229b442b5142c311f49a0
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    Signed-off-by: Rimuru <paleloebarbershop@gmail.com>
    Signed-off-by: neophyte <neophyteplayground@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -6876,7 +6876,7 @@ public class Notification implements Parcelable
         if (extras.getBoolean(EXTRA_REDUCED_IMAGES)) {
             return;
         }
-        boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+        boolean isLowRam = true;
 
         if (mSmallIcon != null
                 // Only bitmap icons can be downscaled.
@@ -7507,7 +7507,7 @@ public class Notification implements Parcelable
         public void reduceImageSizes(Context context) {
             super.reduceImageSizes(context);
             Resources resources = context.getResources();
-            boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+            boolean isLowRam = true;
             if (mPictureIcon != null) {
                 int maxPictureWidth = resources.getDimensionPixelSize(isLowRam
                         ? R.dimen.notification_big_picture_max_height_low_ram
@@ -8555,7 +8555,7 @@ public class Notification implements Parcelable
         public void reduceImageSizes(Context context) {
             super.reduceImageSizes(context);
             Resources resources = context.getResources();
-            boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+            boolean isLowRam = true;
             if (mShortcutIcon != null) {
                 int maxSize = resources.getDimensionPixelSize(
                         isLowRam ? R.dimen.notification_small_icon_size_low_ram
@@ -9614,9 +9614,7 @@ public class Notification implements Parcelable
             super.reduceImageSizes(context);
             if (mVerificationIcon != null) {
                 int rightIconSize = context.getResources().getDimensionPixelSize(
-                        ActivityManager.isLowRamDeviceStatic()
-                                ? R.dimen.notification_right_icon_size_low_ram
-                                : R.dimen.notification_right_icon_size);
+                        R.dimen.notification_right_icon_size_low_ram);
                 mVerificationIcon.scaleDownIfNecessary(rightIconSize, rightIconSize);
             }
         }
