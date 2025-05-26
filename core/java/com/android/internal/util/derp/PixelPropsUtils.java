65ad4d80df393875f545fbe4b92e9491ee646c90
neophyte <168145100+k4ngcaribug@users.noreply.github.com>
2025-05-26 18:58:21 +0700

    PixelPropsUtils: Update spoof fingerprint

--- core/java/com/android/internal/util/derp/PixelPropsUtils.java
+++ core/java/com/android/internal/util/derp/PixelPropsUtils.java
@@ -278,16 +278,16 @@ public final class PixelPropsUtils {
         // Alter build parameters to avoid hardware attestation enforcement
         setPropValue("MANUFACTURER", "Google");
         setPropValue("MODEL", "Pixel 9");
-        setPropValue("FINGERPRINT", "google/tokay_beta/tokay:Baklava/BP22.250124.010/13116449:user/release-keys");
+        setPropValue("FINGERPRINT", "google/tokay_beta/tokay:16/BP31.250502.008/13497110:user/release-keys");
         setPropValue("BRAND", "google");
         setPropValue("PRODUCT", "tokay_beta");
         setPropValue("DEVICE", "tokay");
         setPropValue("VERSION.RELEASE", "Baklava");
-        setPropValue("ID", "BP22.250124.010");
-        setPropValue("VERSION.INCREMENTAL", "13116449");
+        setPropValue("ID", "BP31.250502.008");
+        setPropValue("VERSION.INCREMENTAL", "13497110");
         setPropValue("TYPE", "user");
         setPropValue("TAGS", "release-keys");
-        setPropValue("VERSION.SECURITY_PATCH", "2025-02-05");
+        setPropValue("VERSION.SECURITY_PATCH", "2025-05-05");
         setPropValue("VERSION.DEVICE_INITIAL_SDK_INT", "34");
     }
 
